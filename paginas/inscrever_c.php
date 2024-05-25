c4dfbf56508f2da3de5ca3782181f2c8197f3fc0
DuarteSousa24 <duarte.sousa@ipcbcampus.pt>
2024-05-25 22:11:05 +0100

    Update inscrever_c.php

--- paginas/inscrever_c.php
+++ paginas/inscrever_c.php
@@ -3,16 +3,16 @@ session_start();
 
 include '../basedados/basedados.h';
 
-if(isset($_POST['id_curso'])){
-    $sql = "INSERT INTO inscricoes (id_curso, id_utilizador, estado_inscricao) VALUES (".$_POST['id_curso'].",".$_SESSION['id_utilizador'].",0)";
+if(isset($_POST['id_sala'])){
+    $sql = "update salas set estado_sala = 0 where id_sala = " . $_POST['id_sala'];
     $res = mysqli_query($conn, $sql);
     
     if($res){
-        echo "O seu pedido de inscrição foi realizada com sucesso!";
+        echo "A sua reserva foi realizada com sucesso!";
         header("refresh:2;url=voltar.php");
     }
     else{
-        echo "Erro ao realizar a inscrição!";
+        echo "Erro ao realizar a reservar!";
         header("refresh:2;url=voltar.php");
     }
    
